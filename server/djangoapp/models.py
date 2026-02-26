cfaa13333646d272ca42210c3ece4b547dbfa10b
Alexmi <delacruzalexander818@gmail.com>
2026-02-26 14:25:30 -0500

    CarModel and CarMake models added

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -11,7 +11,15 @@
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# - __str__ method to print a car make 
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +31,49 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+
+    # Many-to-One relationship (una marca → muchos modelos)
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE,
+        related_name="models"
+    )
+
+    # Dealer ID (viene de Cloudant)
+    dealer_id = models.IntegerField()
+
+    # Nombre del modelo
+    name = models.CharField(max_length=100)
+
+    # Tipo de vehículo
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('COUPE', 'Coupe'),
+    ]
+
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
+
+    # Año del modelo
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(1980),
+            MaxValueValidator(2026)
+        ]
+    )
+
+    # Otros campos opcionales
+    color = models.CharField(max_length=50, blank=True)
+    mileage = models.IntegerField(null=True, blank=True)
+    
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
